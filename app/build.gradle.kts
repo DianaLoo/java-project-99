4ddeefbaaca731960f49e10d373edf4a98533fd2
Diana Lomakina <diana_st16@mail.ru>
2025-03-31 11:57:09 +0300

    changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
 	java
+	id("application")
 	id("org.springframework.boot") version "3.4.3"
 	id("io.spring.dependency-management") version "1.1.7"
 	id("checkstyle")
-        id("org.sonarqube") version "6.0.1.5171"
+	id("org.sonarqube") version "6.0.1.5171"
 }
 
 group = "hexlet.code"
@@ -13,6 +14,9 @@ java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(21)
 	}
+}
+application {
+	mainClass = "hexlet.code.App"
 }
 
 repositories {
@@ -25,27 +29,28 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
-tasks.test {
-    useJUnitPlatform()
+//tasks.test {
+   // useJUnitPlatform()
     // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
-    testLogging {
-        exceptionFormat = TestExceptionFormat.FULL
-        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
-        // showStackTraces = true
+    //testLogging {
+       // exceptionFormat = TestExceptionFormat.FULL
+       // events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+       // showStackTraces = true
         // showCauses = true
-        showStandardStreams = true
-    }
-}
-
+       // showStandardStreams = true
+//    }
+//}
+//
 sonar {
-     properties {
-         proper "sonar.projectKey", "DianaLoo_java-project-99"
-         property "sonar.organization", "dianaloo"
-         property "sonar.host.url", "https://sonarcloud.io"
-     }
+	properties {
+		property("sonar.projectKey", "DianaLoo_java-project-99")
+		property("sonar.organization", "dianaloo")
+		property("sonar.host.url", "https://sonarcloud.io")
+	}
 }
