eddfca58824b79fc09a9f0675a5ae728fa9f82e2
Diana Lomakina <diana_st16@mail.ru>
2025-04-02 12:34:49 +0300

    add

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.7"
 	id("checkstyle")
 	id("org.sonarqube") version "6.0.1.5171"
+	id("com.github.ben-manes.versions") version "0.48.0"
+	id("io.freefair.lombok") version "8.6"
 }
 
 group = "hexlet.code"
@@ -28,9 +30,23 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-devtools")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+	implementation("net.datafaker:datafaker:2.0.1")
+	implementation("org.instancio:instancio-junit:3.3.1")
+	runtimeOnly("com.h2database:h2")
+
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+	testImplementation("org.springframework.security:spring-security-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
+	testImplementation(platform("org.junit:junit-bom:5.10.0"))
+	testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+	testImplementation("org.hamcrest:hamcrest:2.2")
 }
 
 tasks.withType<Test> {
---
+++ src/main/java/hexlet/code/controller/UsersController.java
@@ -0,0 +1,75 @@
+package hexlet.code.controller;
+
+import hexlet.code.dto.UserCreateDTO;
+import hexlet.code.dto.UserDTO;
+import hexlet.code.dto.UserUpdateDTO;
+import hexlet.code.exception.ResourceNotFoundException;
+import hexlet.code.mapper.UserMapper;
+import hexlet.code.repository.UserRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class UsersController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping(path = "/users")
+    public List<UserDTO> index() {
+        var users = userRepository.findAll();
+        return users.stream()
+                .map(p -> userMapper.map(p))
+                .toList();
+    }
+
+    @GetMapping(path = "/users/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDTO show(@PathVariable long id) {
+        var user =  userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User with id " + id + " not found"));
+        var userDto = userMapper.map(user);
+        return userDto;
+    }
+
+    @PostMapping(path = "/users")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDTO create(@Valid @RequestBody UserCreateDTO userData) {
+        var user = userMapper.map(userData);
+        userRepository.save(user);
+        // Преобразование в DTO
+        var userDTO = userMapper.map(user);
+        return userDTO;
+    }
+
+    @PutMapping(path = "/users/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDTO update(@PathVariable Long id, @RequestBody UserUpdateDTO userData) {
+        var user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User with id " + id + " not found."));
+        userMapper.update(userData, user);
+        userRepository.save(user);
+        return userMapper.map(user);
+    }
+
+    @DeleteMapping(path = "/users/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable long id) {
+        userRepository.deleteById(id);
+    }
+}
