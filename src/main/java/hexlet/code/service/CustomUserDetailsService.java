7ad7cef3a50d354e07b901df070ba282d315d7fc
Diana Lomakina <diana_st16@mail.ru>
2025-04-30 10:18:40 +0300

    changed

--- src/main/java/hexlet/code/service/CustomUserDetailsService.java
+++ src/main/java/hexlet/code/service/CustomUserDetailsService.java
@@ -5,16 +5,16 @@ import hexlet.code.repository.UserRepository;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
 import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsManager {
 
-    //@Autowired
+    @Autowired
     private UserRepository userRepository;
-    //@Autowired
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
     @Override
